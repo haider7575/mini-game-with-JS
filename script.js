08f22f44eaf09eac963e3d52bb60473ef6178560
haider7575 <haideralishah64672@gmail.com>
2025-03-16 06:53:14 -0700

    comments add

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let boxes = document.querySelectorAll(".box");
             [0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7],
             [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]
         ];
+        // game reset button
         const resetGame = () => {
             turnO = true;
             enableBoxes();
@@ -27,11 +28,13 @@ let boxes = document.querySelectorAll(".box");
                 checkWin();
             });
         });
+        // disables boxes
         const disableBoxes = () => {
             boxes.forEach((box) => {
                 box.disabled = true;
             });
         };
+        // enables boxes again after a winner is found or a draw occurs
         const enableBoxes = () => {
             boxes.forEach((box) => {
                 box.disabled = false;

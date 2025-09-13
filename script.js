643da6de30441bd7ff842dd822d796791f67fee2
haider7575 <haideralishah64672@gmail.com>
2025-09-13 23:57:00 +0500

    ,,,...

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let newGameBtn      = document.querySelector("#new-btn");
 let msgContainer    = document.querySelector(".msg-container");
 let msg             = document.querySelector("#msg");
 let winPatterns     = [
-        [0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, ],
+        [0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7],
         [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]
 ];
 // set boxes with o and x
